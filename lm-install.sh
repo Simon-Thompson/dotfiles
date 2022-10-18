893e6c7b906291f81b746c2ca8cec7774f348a1d
Simon-Thompson <simon.alexander.thompson@gmail.com>
2022-10-18 00:05:36 -0400

    modified lm install to be simpler

--- lm-install.sh
+++ lm-install.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 # Login manager
-git clone --recurse-submodules https://github.com/nullgemm/ly.git
-cd ly/
-make
-sudo make install
+yay -S ly
 sudo systemctl enable ly.service
 sudo systemctl disable getty@tty2.service
-cd ..
-rm -rf ly
-cd ~
-
-# Enable lightdm
-sudo systemctl enable lightdm
-chmod +x autorun.sh
